bf76ba2e6d24ae49318f0a67c3c7cd92f6248e38
Metehan <65496622+tneilc@users.noreply.github.com>
2021-12-31 13:40:53 -0800

    Minor Changes
    Added language selection

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 import * as vscode from "vscode";
 import "./helpers/utils";
 import { quickPick } from "./helpers/utils";
-
+import { data } from "./templates/templates";
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand("filesnippets.CreateFiles", async () => {
@@ -10,20 +10,31 @@ export function activate(context: vscode.ExtensionContext) {
   );
 }
 
-function createWorkspacePicker(){
+function createWorkspacePicker() {
   let ws = vscode.workspace.workspaceFolders;
-  if(!ws) {return;};
-  if(ws.length === 1){
-
-  } ;
-  let wsPaths : vscode.QuickPickItem[] = ws.map(obj => ({label : obj.uri.fsPath}));
-  let workspacePicker = quickPick(wsPaths,(e) => {
-    vscode.window.showInformationMessage(e[0].label);
+  if (!ws) {
+    return;
+  }
+  if (ws.length === 1) {
+    createLanguagePicker(ws[0].uri.fsPath);
+    return;
+  }
+  let wsPaths: vscode.QuickPickItem[] = ws.map((obj) => ({
+    label: obj.uri.fsPath,
+  }));
+  let workspacePicker = quickPick(wsPaths, (e) => {
+    createLanguagePicker(e[0].label);
   });
   workspacePicker.show();
 }
-function createLanguagePicker(path:string){
-  
+function createLanguagePicker(path: string) {
+  const languageNames: vscode.QuickPickItem[] = Object.keys(data.languages).map(
+    (key) => ({ label: key })
+  );
+  let languagePicker = quickPick(languageNames, (e) => {
+    vscode.window.showInformationMessage(path+e[0].label);
+  });
+  languagePicker.show();
 }
 
-export function deactivate() {}
+export function deactivate(): void {}
---
+++ src/templates/templates.ts
@@ -0,0 +1,12 @@
+export const data = {
+  languages: {
+    javascriptreact: {
+      files: [".js", ".css"],
+      import: 'import "./{}"',
+    },
+    typescriptreact: {
+      files: [".ts", ".css"],
+      import: 'import "./{}"',
+    },
+  },
+};
