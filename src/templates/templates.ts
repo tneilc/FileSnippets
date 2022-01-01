17cd3b1e59372c8d7fbaa5fe28caccb828b63e14
Metehan <65496622+tneilc@users.noreply.github.com>
2022-01-01 06:40:19 -0800

    Minor Fix
    Css extension added to import

--- src/templates/templates.ts
+++ src/templates/templates.ts
@@ -1,18 +1,19 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 export const data = {
   languages: {
-    javascriptcss: {
+    JavascriptCss: {
       files: [".js", ".css"],
-      import: 'import "./{0}"',
+      import: 'import "./{0}.css"',
     },
-    typescriptcss: {
+    TypescriptCss: {
       files: [".ts", ".css"],
-      import: 'import "./{0}"',
+      import: 'import "./{0}.css"',
     },
-    cpp:{
+    Cpp:{
       files: [".cpp",".h"],
       import : "#include \"{0}.h\""
     },
-    html:{
+    Html:{
       files: [".html",".css"],
       import : '<!DOCTYPE html>\n<html>\n  <head>\n    <link rel=\"stylesheet\" href=\"{0}.css\" />\n  </head>\n  <body>\n    <h1>FileSnippets Says Hello!</h1>\n  </body>\n</html>\n'
     }
