1910136251af747719b9d90f56a4d0c14e820957
Metehan <65496622+tneilc@users.noreply.github.com>
2022-01-01 06:37:45 -0800

    Added template
    C++ and HTML

--- src/templates/templates.ts
+++ src/templates/templates.ts
@@ -1,12 +1,20 @@
 export const data = {
   languages: {
-    javascriptreact: {
+    javascriptcss: {
       files: [".js", ".css"],
       import: 'import "./{0}"',
     },
-    typescriptreact: {
+    typescriptcss: {
       files: [".ts", ".css"],
       import: 'import "./{0}"',
     },
+    cpp:{
+      files: [".cpp",".h"],
+      import : "#include \"{0}.h\""
+    },
+    html:{
+      files: [".html",".css"],
+      import : '<!DOCTYPE html>\n<html>\n  <head>\n    <link rel=\"stylesheet\" href=\"{0}.css\" />\n  </head>\n  <body>\n    <h1>FileSnippets Says Hello!</h1>\n  </body>\n</html>\n'
+    }
   },
 };
